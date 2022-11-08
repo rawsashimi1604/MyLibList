52b412cead77d5335d98ea10fad8be78ceb14a93
junweilam <91267859+junweilam@users.noreply.github.com>
2022-11-08 14:09:13 +0800

    cleanSQPL

--- seedDb/cleanPSQL.js
+++ seedDb/cleanPSQL.js
@@ -1,5 +1,7 @@
-import setupEnv from "./lib/utils/setupEnv.js";
+import setupEnv from "../src/lib/utils/setupEnv.js";
 import databases from "../db/index.js";
+import fs from "fs";
+import { parse } from "csv-parse";
 
 // Set up dotenv environment from .env file.
 setupEnv("../../.env");
@@ -9,3 +11,16 @@ const database = databases.PSQLDatabase;
 
 // Insert cleaning algorithm here...
 // ...
+
+fs.createReadStream("../seedDb/csv_data/nlb_data.csv")
+  .pipe(parse({ delimiter: ",", from_line: 2 }))
+  .on("data", function (row) {
+    console.log(row);
+  })
+
+  .on("end", function () {
+    console.log("finished");
+  })
+  .on("error", function (error) {
+    console.log(error.message);
+  });
