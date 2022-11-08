af06abb038a8695fb08cb2029daae51a3cd9fa08
Gavin Loo <looweiren@gmail.com>
2022-11-08 14:20:07 +0800

    added some scripts for cleaning csv data

--- seedDb/cleanPSQL.js
+++ seedDb/cleanPSQL.js
@@ -12,15 +12,40 @@ const database = databases.PSQLDatabase;
 // Insert cleaning algorithm here...
 // ...
 
+// [ {}, {}, {} ]
+const cleanedData = [];
+
+/*
+  uuid: 0,
+  uri: 1,
+  alternative_titles: 5
+*/
+
 fs.createReadStream("../seedDb/csv_data/nlb_data.csv")
   .pipe(parse({ delimiter: ",", from_line: 2 }))
   .on("data", function (row) {
-    console.log(row);
+
+    // All the keys in ur object
+    let uuid = row[0];
+    let uri = row[1];
+    let alternative_titles = []
+
+    // Logic 
+    if(row[5] !== "NA") {
+      alternative_titles = row[5].split("|");
+    }
+
+    const cleanedDataRow = {
+      uuid, uri, alternative_titles
+    }
+    cleanedData.push(cleanedDataRow)
   })
 
   .on("end", function () {
     console.log("finished");
+    console.log(cleanedData)
   })
   .on("error", function (error) {
     console.log(error.message);
   });
+
