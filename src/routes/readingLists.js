d8d5ba3639f24e4300ba6f58fc59a91494ca478f
Gavin Loo <looweiren@gmail.com>
2022-11-15 21:32:05 +0800

    fix merge conflict

--- src/routes/readingLists.js
+++ src/routes/readingLists.js
@@ -12,18 +12,18 @@ export default function (database) {
   router.use((req, res, next) => injectDatabase(req, res, next, database));
 
   // Routes
-<<<<<<< HEAD
   router.get(
     "/:readingListID",
     asyncErrorHandler(ReadingListsController.handleGetSpecificReadingList)
   );
-=======
->>>>>>> 35f19a0ca68e2cf41b219aa743c8034ecf101cdb
   router.get(
     "/all",
     asyncErrorHandler(ReadingListsController.handleAllReadingList)
   );
-  router.get("/:readingListID", asyncErrorHandler(ReadingListsController.handleGetSpecificReadingList));
+  router.get(
+    "/:readingListID",
+    asyncErrorHandler(ReadingListsController.handleGetSpecificReadingList)
+  );
   router.post(
     "/addReadingList",
     asyncErrorHandler(ReadingListsController.handleAddReadingList)
