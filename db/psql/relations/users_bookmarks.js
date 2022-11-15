627cac3853746f908510538fad79173e36951cfe
junweilam <91267859+junweilam@users.noreply.github.com>
2022-11-15 21:20:03 +0800

    updated books

--- db/psql/relations/users_bookmarks.js
+++ db/psql/relations/users_bookmarks.js
@@ -21,15 +21,27 @@ function getUsersBookmarksByID(book_uuid) {
   }
 }
 
+function checkIfBookmarkAdded(user,book){
+  try{
+    const query = `SELECT * FROM "users_bookmarks" WHERE email = $1 AND book_uuid = $2`;
+    const params = [user, book];
+    return db.query(query, params);
+  }catch (err){
+    console.log(err);
+    throw err;
+  }
+}
+
 function addBookUserBookmarks(book) {
   try {
     const query = `INSERT INTO "users_bookmarks"(
       email,
       book_uuid,
-      timestamp_liked,
-    ) VALUES ($1, $2, $3)`;
+      page,
+      timestamp_bookmarked
+    ) VALUES ($1, $2, $3, $4)`;
 
-    const params = [book.email, book.book_uuid, book.timestamp_liked];
+    const params = [book.email, book.book_uuid, book.page, book.timestamp_bookmarked];
 
     return db.query(query, params);
   } catch (err) {
@@ -38,8 +50,22 @@ function addBookUserBookmarks(book) {
   }
 }
 
+function updateBookmark(page, timestamp, email, book){
+  try{
+    const query = `UPDATE "users_bookmarks" SET page = $1, timestamp_bookmarked = $2 WHERE email = $3 AND book_uuid = $4`;
+    const params = [page, timestamp, email, book];
+    return db.query(query, params);
+  } catch (err){
+    console.log(err);
+    throw err;
+  }
+}
+
+
 export default {
   getAllUsersBookmarks,
   getUsersBookmarksByID,
   addBookUserBookmarks,
+  checkIfBookmarkAdded,
+  updateBookmark,
 };
